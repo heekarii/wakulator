7c8d14703145d00f13b9b84b9c36e1e705294cf3
kms0219kms <kms0219kms@naver.com>
2025-01-24 11:48:29 +0700

    :speech_balloon: feat: 2025년 새해가 밝았습니다

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(props: { readonly characterIndex: number }) {
 
   return (
     <FooterStyle>
-      <FooterStyle.Text>© 2023-2024 WAKULATOR. All Rights Reserved.</FooterStyle.Text>
+      <FooterStyle.Text>© 2023-2025 WAKULATOR. All Rights Reserved.</FooterStyle.Text>
       <FooterStyle.Text>
         Made by heekari, 이파리맛별사탕, Clouuud, Ayaan, SY with{" "}
         <FooterStyle.Text.Emoji>{character[props.characterIndex]}</FooterStyle.Text.Emoji>
