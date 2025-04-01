22945a987bd4d743bdd3144898b0e42c51699f33
Minsu Kim <minsu_kim@bishanoi.net>
2025-04-01 11:00:07 +0700

    :egg: feat: 우왁끼는 살아있다!!!

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
 import { FooterStyle } from "~/styles/components/footer"
 
 export default function Footer(props: { readonly characterIndex: number }) {
-  const character = ["💜", "💛", "🖤", "❤", "💙", "💚"]
+  // const character = ["💜", "💛", "🖤", "❤", "💙", "💚"]
 
   return (
     <FooterStyle>
-      <FooterStyle.Text>© 2023-2025 WAKULATOR. All Rights Reserved.</FooterStyle.Text>
-      <FooterStyle.Text>
+      <FooterStyle.Text>© 2019-2021 WAKULATOR. All Rights Reserved.</FooterStyle.Text>
+      {/*<FooterStyle.Text>
         Made by heekari, 이파리맛별사탕, Clouuud, Ayaan, SY with{" "}
         <FooterStyle.Text.Emoji>{character[props.characterIndex]}</FooterStyle.Text.Emoji>
-      </FooterStyle.Text>
+      </FooterStyle.Text>*/}
+      <FooterStyle.Text>우왁끼는 살아있다!!!</FooterStyle.Text>
     </FooterStyle>
   )
 }
